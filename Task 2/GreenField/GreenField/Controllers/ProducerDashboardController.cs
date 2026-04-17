fc41f92f9c638d0c000542650ac7329a8c21b313
Hfbwaadwfawf <rexxxits@gmail.com>
2026-04-17 13:22:01 +0100

    producer dashboard made

---
+++ Task 2/GreenField/GreenField/Controllers/ProducerDashboardController.cs
@@ -0,0 +1,34 @@
+﻿using System.Security.Claims;
+using GreenField.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace GreenField.Controllers
+{
+    [Authorize(Roles = "Producer")]
+    public class ProducerDashboardController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        public ProducerDashboardController (ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var UserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var Producer = await _context.Producers.FirstOrDefaultAsync(p => p.UserId == UserId);
+            if (Producer == null)
+            {
+                return NotFound();
+            }
+            var products = await _context.Products.Where (x => x.ProducersId == Producer.ProducersId).ToListAsync();
+            var orders = await _context.Orders.Include(o => o.OrderProducts).ThenInclude(op => op.Products).Where(o => o.OrderProducts.Any(op => op.Products.ProducersId == Producer.ProducersId)).ToListAsync();
+            ViewBag.TotalProducts = products.Count;
+            ViewBag.LowStockCount = products.Count(x =>x.Stock <= 5);
+            ViewBag.RecentOrders = orders;
+
+            return View();
+        }
+    }
+}
--- Task 2/GreenField/GreenField/obj/Debug/net9.0/GreenField.AssemblyInfo.cs
+++ Task 2/GreenField/GreenField/obj/Debug/net9.0/GreenField.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenField")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2236f28f9fb33e3ca7fc487e382e88332303eb94")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+678e762b7d2deea32aafda31b1cba7ce89b67af7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenField")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenField")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
