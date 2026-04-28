149db739efc3ebc85bf118ae894ad6fcba159306
Hfbwaadwfawf <rexxxits@gmail.com>
2026-04-28 10:18:45 +0100

    .

--- GreenField/GreenField/obj/Debug/net9.0/GreenField.AssemblyInfo.cs
+++ GreenField/GreenField/obj/Debug/net9.0/GreenField.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GreenField")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b34df17a04ab925d2bd4d118030a10032fe69982")]
 [assembly: System.Reflection.AssemblyProductAttribute("GreenField")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GreenField")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
